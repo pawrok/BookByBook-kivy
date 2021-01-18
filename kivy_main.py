1d7b096855489729fd41f2837e88c21f1a99b6f2
Pablo <pawelrokicki@yahoo.com>
2021-01-18 22:05:32 +0100

    redesign containers

--- kivy_main.py
+++ kivy_main.py
@@ -6,20 +6,26 @@ from kivy.properties import ListProperty, StringProperty, ObjectProperty, \
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 
 import kivy
+
 kivy.require('1.8.0')
 
 class BookItem(BoxLayout):
     book_title = StringProperty()
 
+
 class BottomMenu(BoxLayout):
     book_title = StringProperty()
+
 
 class ShelfItem(BoxLayout):
     shelf_title = StringProperty()
 
+
 class RootWidget(BoxLayout):
-    container = ObjectProperty(None)
-    container2 = ObjectProperty(None)
+    upper_container = ObjectProperty(None)
+    bottom_container = ObjectProperty(None)
+    home_container = ObjectProperty(None)
+
 
 class BookWidget(BoxLayout):
     data = ListProperty()
@@ -27,17 +33,12 @@ class BookWidget(BoxLayout):
         return self.data
     data_for_widgets = AliasProperty(_get_data_for_widgets, bind=['data'])
 
+
 class ShelfWidget(BoxLayout):
     data = ListProperty()
     def _get_data_for_widgets(self):
         return self.data
     data_for_widgets = AliasProperty(_get_data_for_widgets, bind=['data'])
-
-class HomeScreen(Screen):
-    homex = Builder.load_file('kv/root.kv')
-
-class AddBookScreen(Screen):
-    pass
 
 
 class BookcaseApp(App):
@@ -47,31 +48,29 @@ class BookcaseApp(App):
         self.shelf_store = ShelfWidget()
         self.load_shelves()
         self.root = Builder.load_file('kv/root.kv')
-        self.createBottomMenu()
-
+        self.create_bottommenu()
 
     def next_screen(self, screen):
         filename = screen + '.kv'
         Builder.unload_file('kv/' + filename)
-        self.root.container.clear_widgets()
+        self.root.upper_container.clear_widgets()
         screen = Builder.load_file('kv/' + filename)
-        self.root.container.add_widget(screen)
+        self.root.upper_container.add_widget(screen)
 
-    def MenuAddNote(self):
-        self.root.clear_widgets()
-        screen = Builder.load_file('kv/addBook.kv')
-        self.root.add_widget(screen)
-
-    def createBottomMenu(self):
-        Builder.unload_file('kv/bottomMenu.kv')
-        self.root.container2.clear_widgets()
+    # TODO: create one method for bottom two ones
+    def create_bottommenu(self):
+        Builder.unload_file('kv/root.kv')
+        self.root.bottom_container.clear_widgets()
         screen = Builder.load_file('kv/bottomMenu.kv')
-        self.root.container2.add_widget(screen)
+        self.root.bottom_container.add_widget(screen)
+
+    def create_newbookmenu(self):
+        Builder.unload_file('kv/root.kv')
+        self.root.home_container.clear_widgets()
+        screen = Builder.load_file('kv/addBook.kv')
+        self.root.home_container.add_widget(screen)
 
     def load_books(self):
-        # if not exists(self.notes_fn):
-        #     return
-        # with open(self.notes_fn) as fd:
         data = [{
             'book_index': 5,
             'book_content': 'contentxxx',
@@ -79,9 +78,6 @@ class BookcaseApp(App):
         self.book_store.data = data
 
     def load_shelves(self):
-        # if not exists(self.notes_fn):
-        #     return
-        # with open(self.notes_fn) as fd:
         data = [{
             'shelf_title': 'random shelf'}]
         self.shelf_store.data = data
