c77ed79bb9e808fbb6b563d14da6fe8a5e5c4cce
Pablo <pawelrokicki@yahoo.com>
2021-01-30 20:59:08 +0100

    can now add and edit books

--- kivy_main.py
+++ kivy_main.py
@@ -18,7 +18,7 @@ import kivy
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '650')
 
-kivy.require('1.9.0')
+# kivy.require('1.9.0')
 
 class RootRoot(BoxLayout):
     pass
@@ -49,13 +49,31 @@ class StatsScreen(Screen):
     pass
 
 class AddScreen(Screen):
-    pass
+    input_title = StringProperty("Title")
+    input_author = StringProperty("Author")
+    input_category = StringProperty("Category")
+    input_rating = NumericProperty(0)
+    input_rented_person = StringProperty("Borrowed to...")
+    input_date_completed = StringProperty("1.01.2021")
+    input_pages = NumericProperty(0)
+    book_id = NumericProperty(0)
+    def reset_properties(self):
+        self.input_title = "Title"
+        self.input_author = "Author"
+        self.input_category = "Category"
+        self.input_rating = 0
+        self.input_rented_person = "Borrowed to..."
+        self.input_date_completed = "1.01.2021"
+        self.input_pages = 0
+        self.book_id = 0
+     
 
 class WishScreen(Screen):
     pass
 
 class SettingsScreen(Screen):
-    pass
+    def delete_DB(self):
+        SqliteDB.delete_table()
 
 class Books(Screen):
     pass
@@ -71,19 +89,58 @@ class GridLayoutTest(GridLayout):
 
 class BookItem(BoxLayout):
     title = StringProperty()
+    book_id = NumericProperty()
+    
+class StarsButton(BoxLayout):
+    rating = NumericProperty()
+    # def __init__(self, **kwargs):
+    #     super(StarsButton, self).__init__(**kwargs)
+    #     self.set_rating(self.rating)
+    #     print(self.rating)
+
+    def set_rating(self, value):
+        if value == 1:
+            self.ids['first_s'].background_color = 1.0, 0.0, 1.0, 1.0
+            self.ids['sec_s'].background_color = 1.0, 1.0, 1.0, 1.0
+            self.ids['third_s'].background_color = 1.0, 1.0, 1.0, 1.0
+            self.ids['fourth_s'].background_color = 1.0, 1.0, 1.0, 1.0
+            self.rating = 1
+        elif value == 2:
+            self.ids['first_s'].background_color = 1.0, 0.0, 1.0, 1.0
+            self.ids['sec_s'].background_color = 1.0, 0.0, 1.0, 1.0
+            self.ids['third_s'].background_color = 1.0, 1.0, 1.0, 1.0
+            self.ids['fourth_s'].background_color = 1.0, 1.0, 1.0, 1.0
+            self.rating = 2
+        elif value == 3:
+            self.ids['first_s'].background_color = 1.0, 0.0, 1.0, 1.0
+            self.ids['sec_s'].background_color = 1.0, 0.0, 1.0, 1.0
+            self.ids['third_s'].background_color = 1.0, 0.0, 1.0, 1.0
+            self.ids['fourth_s'].background_color = 1.0, 1.0, 1.0, 1.0
+            self.rating = 3
+        elif value == 4:
+            self.ids['first_s'].background_color = 1.0, 0.0, 1.0, 1.0
+            self.ids['sec_s'].background_color = 1.0, 0.0, 1.0, 1.0
+            self.ids['third_s'].background_color = 1.0, 0.0, 1.0, 1.0
+            self.ids['fourth_s'].background_color = 1.0, 0.0, 1.0, 1.0
+            self.rating = 4
+        else:
+            pass
+
+        
 
 class ShelfItem(BoxLayout):
     shelf_title = StringProperty()
 
 class BookScrollView(ScrollView):
-    def __init__(self, **kwargs): 
-        super(BookScrollView, self).__init__(**kwargs) 
+    def __init__(self, **kwargs):
+        super(BookScrollView, self).__init__(**kwargs)
+        
         self.data = SqliteDB.get_books_fromDB()
-        #[{'ID': 2, 'title': 'title1'}, {'ID': 1, 'title': 'title2'}, {'ID': 1, 'title': 'title2'}, {'ID': 1, 'title': 'title2'}, {'ID': 1, 'title': 'title2'}]
 
-        layout = GridLayout(cols=3, spacing=15, size_hint_y=None, col_default_width = self.width / 3)
+        #TODO: height below is fixed, but should be dynamic
+        layout = GridLayout(cols=3, spacing=15, size_hint_y=None, height=900, col_default_width = self.width / 3)
         for item in self.data:
-            book = BookItem(title=item['title'])
+            book = BookItem(book_id=item['ID'],title=item['title'])
             layout.add_widget(book)
         self.add_widget(layout)
 
@@ -92,10 +149,72 @@ class BookcaseApp(App):
     def build(self):
         SqliteDB()
         Builder.load_file('kv/root.kv')
-        return RootRoot()
+        self.root = RootRoot()
+        return self.root
 
-    def add_book_to_db(self, input_title, input_author):
-        SqliteDB.add_book_toDB(5, input_title, input_author)
+    def add_book_to_db(
+            self, input_title, input_author, input_category, input_rating, input_rented_person,
+            input_date_completed, input_pages):
+        
+        if input_rented_person:
+            is_rent = True
+        
+        print("add")
+        input_rating = int(input_rating)
+        is_rent = int(is_rent)
+        input_pages = int(input_pages)
+        
+        SqliteDB.add_book_toDB(
+            input_title, input_author, input_category, input_rating,
+            is_rent, input_rented_person, input_date_completed, input_pages)
+    
+    def update_book_in_db(
+            self, id, input_title, input_author, input_category, input_rating,
+            input_rented_person, input_date_completed, input_pages):
+        
+        if input_rented_person:
+            is_rent = True
+
+        print("update")
+        
+        input_rating = int(input_rating)
+        is_rent = int(is_rent)
+        input_pages = int(input_pages)
+        
+        SqliteDB.edit_book_inDB(
+            id, input_title, input_author, input_category, input_rating,
+            is_rent, input_rented_person, input_date_completed, input_pages)
+        
+    def add_home_screen(self):
+        index = 0
+        for i in range(len(self.root.ids.rootmanager.screen_names)):
+            if self.root.ids.rootmanager.screen_names[i].find('home') != -1:
+                index = i
+        
+        self.root.ids['rootmanager'].remove_widget(self.root.ids.rootmanager.screens[index])
+        self.root.ids['rootmanager'].add_widget(HomeScreen())
+
+    def open_edit_book(self, id):
+        for i in range(len(self.root.ids.rootmanager.screen_names)):
+            if self.root.ids.rootmanager.screen_names[i].find('new') != -1:
+                index = i
+        
+        book_values = SqliteDB.get_single_book_fromDB(id)
+        self.root.ids['rootmanager'].remove_widget(self.root.ids.rootmanager.screens[index])
+        self.root.ids['rootmanager'].add_widget(AddScreen(
+            input_title = book_values['title'],
+            input_author = book_values['author'],
+            input_category = book_values['category'],
+            input_rating = book_values['rating'],
+            input_rented_person = book_values['rentedPerson'],
+            input_date_completed = book_values['dateCompleted'],
+            input_pages = book_values['pageCount'],
+            book_id = id
+            ))
+        # self.root.ids['rootmanager'].screens[4].children[0].children[0].children[1].children[1].children[1]
+        self.root.ids['rootmanager'].current = 'new'
+        #                          AddScreen -> BoxLayout -> BoxLayout -> BoxLayout -> BoxLayout -> StarsButton -> set_rating
+        self.root.ids['rootmanager'].screens[4].children[0].children[0].children[1].children[1].children[1].set_rating(book_values['rating'])
 
 
 if __name__ == '__main__':
--- sqliteDB.py
+++ sqliteDB.py
@@ -11,6 +11,7 @@ class SqliteDB:
         if SqliteDB.__instance__ is None:
             SqliteDB.__instance__ = self
             SqliteDB.c, SqliteDB.conn = SqliteDB.start_connection()
+            SqliteDB.create_table()
         else:
             raise Exception("You cannot create another SqliteDB class")
 
@@ -44,24 +45,54 @@ class SqliteDB:
         )''' % table_name)
 
     def add_book_toDB(
-            ID, title, author, category = '', rating = 0, \
+            title, author, category = '', rating = 0, \
             isRent = 0, rentedPerson = '', dateCompleted = '', \
             pageCount = 0):
-        SqliteDB.c.execute("INSERT INTO booktable VALUES ('%s', '%s', '%s', '%s', '%d', '%r', '%s', '%s', '%d')" % \
-            (ID, title, author, category, rating, isRent, rentedPerson, dateCompleted, pageCount))
+        # set ID to max(ID) + 1; fetchone() sometimes haven't worked
+        SqliteDB.c.execute("SELECT MAX(ID) FROM booktable")
+        xxx = [dict(row) for row in SqliteDB.c.fetchall()]
+        try:
+            max_id = xxx[0]['MAX(ID)'] + 1
+        except:
+            max_id = 1
+        SqliteDB.c.execute("INSERT INTO booktable VALUES ('%d', '%s', '%s', '%s', '%d', '%d', '%s', '%s', '%d')" % \
+            (max_id, title, author, category, rating, isRent, rentedPerson, dateCompleted, pageCount))
         SqliteDB.conn.commit()
 
     def get_books_fromDB():
         SqliteDB.c.execute('SELECT * FROM booktable')
-        # result = 
         return [dict(row) for row in SqliteDB.c.fetchall()]
+
+    def get_single_book_fromDB(ID):
+        SqliteDB.c.execute('SELECT * FROM booktable WHERE ID = %d' % ID)
+        selected_book = SqliteDB.c.fetchone()
+        if selected_book != None:
+            return dict(selected_book)
+        else:
+            return 0
 
     def del_book_fromDB(ID):
         SqliteDB.c.execute('DELETE FROM booktable where ID = %d' % ID)
         SqliteDB.conn.commit()
 
-    def edit_book_inDB(ID, param, new_val):
-        SqliteDB.c.execute("UPDATE booktable SET %s = '%s' where ID = %d" % param, new_val, ID)
+    def edit_book_inDB(ID, title, author, category = '', rating = 0, \
+            isRent = 0, rentedPerson = '', dateCompleted = '', \
+            pageCount = 0):
+        sql = """
+            UPDATE booktable SET 
+                title = ?,
+                author = ?,
+                category = ?,
+                rating = ?,
+                isRent = ?,
+                rentedPerson = ?,
+                dateCompleted = ?,
+                pageCount = ?
+            WHERE 
+                ID = ?
+            """
+        SqliteDB.c.execute(sql, (title, author, category, rating, isRent, 
+                                rentedPerson, dateCompleted, pageCount, ID))
         SqliteDB.conn.commit()
 
     def sort_books(books, param, rev):
@@ -70,9 +101,25 @@ class SqliteDB:
     def filter_books(books, param, value):
         return [tpl for tpl in books if tpl[param] == value]
 
-# SqliteDB()
-# SqliteDB.create_table()
-# SqliteDB.add_book_toDB(1, 'title1', 'author1')
-# SqliteDB.del_book_fromDB(1)
-# print(SqliteDB.get_books_fromDB())
-# print(sort_books(get_books_fromDB(c), 7, True))
+    #  ------------- DEVELOPER FUNCTIONS ------------------
+    def delete_table():
+        SqliteDB.c.execute("DROP TABLE booktable")
+        SqliteDB.conn.commit()
+        SqliteDB.create_table()
+
+
+
+debug_mode = 0
+if debug_mode:
+    SqliteDB()
+    # SqliteDB.create_table()
+    # SqliteDB.add_book_toDB(1, 'title1', 'author1')
+    # SqliteDB.del_book_fromDB(1)
+    # SqliteDB.c.execute("SELECT * FROM booktable")
+    # print(SqliteDB.c.fetchall())
+    # SqliteDB.c.execute("SELECT MAX(ID) FROM booktable")
+    # xxx = [dict(row) for row in SqliteDB.c.fetchall()]
+    # print(xxx[0]['MAX(ID)'])
+    # print(SqliteDB.get_single_book_fromDB(555))
+    print(SqliteDB.get_books_fromDB())
+ 
